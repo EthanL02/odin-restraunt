3c3a80527bfb30dec4d0d29f64e0ed543bfb223d
Ethan Lopez <102267044+EthanL02@users.noreply.github.com>
2025-09-22 19:51:19 -0400

    Create module to load content with home page

--- src/create-home.js
+++ src/create-home.js
@@ -4,7 +4,15 @@ function load() {
     const title = document.createElement("h1");
     title.textContent = "Restraunt Name";
 
+    const description = document.createElement("h3");
+    description.textContent = "Sample description of a restraunt";
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "Some more sample text that doesn't really say anything";
+
     content.append(title);
+    content.append(description);
+    content.append(paragraph);
 }
 
 export default load;
